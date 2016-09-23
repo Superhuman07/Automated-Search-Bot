5c36db1843f65f910252669d44a2de22cadc0138
Rahul Bhatia <Superhuman07@users.noreply.github.com>
2016-09-23 21:37:25 +0530

    Add Comments

--- search.py
+++ search.py
@@ -1,11 +1,14 @@
 import webbrowser
+'''
+Chnage the serach_term for your own use later it will be appended by the append operation in the while loop
+'''
 search_term = 'h'
 num = 0
 def function(search_term):
     url = "https://www.bing.com/search?q={}".format(search_term)    
     webbrowser.open(url)
 '''
-calling function 10 times with new strings
+calling function 10 times with new strings change the limit to request more from the search engine
 '''
 while num < 10:
     search_term += 'l'
