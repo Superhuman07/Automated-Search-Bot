06300933233322940c99706e85870cd43fcd3faf
Rahul Bhatia <Superhuman07@users.noreply.github.com>
2020-10-17 13:21:59 +0530

    Update search.py
    added multiple web browser checking functionality to check the whether the browser is installed or not

--- search.py
+++ search.py
@@ -1,16 +1,30 @@
+#importing the windows client app
+
+import win32com.client
+x = win32com.client.Dispatch("AppRobotic.API")
+# importing the webbrowser library
 import webbrowser
-'''
-Chnage the serach_term for your own use later it will be appended by the append operation in the while loop
-'''
-search_term = 'h'
-num = 0
-def function(search_term):
-    url = "https://www.bing.com/search?q={}".format(search_term)    
-    webbrowser.open(url)
-'''
-calling function 10 times with new strings change the limit to request more from the search engine
-'''
-while num < 10:
-    search_term += 'l'
-    function(search_term)
-    num += 1
+
+# specify URL
+url = "https://www.google.com"
+
+# open with default browser maybe windows explorer but the user can change the default web browser to chrome
+webbrowser.open_new(url) 
+
+#checking the webbrowser if they are installed or not 
+# open with Safari, if installed
+webbrowser.get('safari').open_new(url) 
+# open with Firefox, if installed
+webbrowser.get('firefox').open_new_tab(url) 
+# open with Chrome, if installed
+webbrowser.get(using='google-chrome').open_new(url)
+
+# wait a bit for page to open for 3 seconds
+x.Wait(3000)
+# use UI Item Explorer to find the X,Y coordinates of Search box
+x.MoveCursor(438, 435)
+# click inside Search box
+x.MouseLeftClick
+
+x.Type("AppRobotic")
+x.Type("{ENTER THE REQUIRED STRING YOU WANT TO SEARCH}")
